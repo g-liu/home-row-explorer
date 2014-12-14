28cf07a7a98fcdbb59bdedeae689bdd7bb2b4e42
Geoffrey Liu <liug.2012@gmail.com>
2014-12-13 16:35:39 -0800

    numInTop function

--- rowstats.py
+++ rowstats.py
@@ -9,4 +9,22 @@ def getLengthDistribution(wordList):
 
 	return lengths
 
-print(getLengthDistribution(fileinput.input()))
+# Returns the number of words in the top n most frequently used words
+def numInTop(n):
+	f = open(sys.argv[1], 'r')
+	numInTop = 0
+	for i in range(0, n):
+		word = f.readline().strip().split("\t")[0]
+		if word in word_list:
+			numInTop += 1
+
+	f.close()
+	return numInTop
+
+if len(sys.argv) != 4:
+	print("Usage: py rowstats.py <word-freq-file> <word-list> <top-n-count>")
+	exit(len(sys.argv))
+
+word_list = set(line.strip() for line in open(sys.argv[2]))
+
+print(numInTop(int(sys.argv[3])))
